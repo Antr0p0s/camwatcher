55ff4626449f3553c4e6a33c73f7a56c57b3c284
Antr0p0s <56258274+Antr0p0s@users.noreply.github.com>
2026-04-21 11:03:39 +0200

    fx

--- backup_trigger.py
+++ backup_trigger.py
@@ -14,3 +14,4 @@ headers = {"Authorization": f"Bearer {AUTH_KEY}"}
 FORCE_BACKUP = True
 
 success_mp4 = backup_to_s3(API_URL)
+trigger_server_compilation(API_URL, True, True, [], 'triggered_backup')
